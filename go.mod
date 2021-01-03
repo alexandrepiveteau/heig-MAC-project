14a9e934f7b6d698bb053d964b629456ffb7bed1
Alexandre Piveteau <alexandre@piveteau.email>
2021-01-03 12:46:59 +0100

    Connect to Neo4j with the official Go Driver

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/neo4j/neo4j-go-driver/v4 v4.2.0 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 )
--- pkg/main.go
+++ pkg/main.go
@@ -2,21 +2,30 @@ package main
 
 import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 	"log"
 	"os"
 )
 
 const envDebug = "BOT_DEBUG"
 const envToken = "BOT_TOKEN"
+const envNeo4j = "BOT_NEO4J"
 
 func main() {
 	debug := os.Getenv(envDebug)
 	token := os.Getenv(envToken)
+	neo4jHost := os.Getenv(envNeo4j)
 
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Panic(err)
 	}
+
+	driver, err := neo4j.NewDriver(neo4jHost, neo4j.NoAuth())
+	if err != nil {
+		log.Panic(err)
+	}
+	defer driver.Close()
 
 	bot.Debug = debug == "true"
 	log.Printf("Authorized on account %s", bot.Self.UserName)
@@ -35,6 +44,11 @@ func main() {
 
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 		msg.ReplyToMessageID = update.Message.MessageID
+
+		// Create a placeholder session, to test Neo4j connectivity.
+		session := driver.NewSession(neo4j.SessionConfig{})
+		_ = session.Close()
+
 		_, _ = bot.Send(msg)
 	}
 }
