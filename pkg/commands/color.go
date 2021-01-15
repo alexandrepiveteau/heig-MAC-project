232fb077604c0941658185a5636a0092a038292a
Guy-Laurent Subri <guy-laurent@subri.ch>
2021-01-15 17:34:51 +0100

    Add inline keyboard

--- pkg/commands/color.go
+++ pkg/commands/color.go
@@ -6,6 +6,19 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+var colorKBD = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Red", "red"),
+		tgbotapi.NewInlineKeyboardButtonData("Green", "green"),
+		tgbotapi.NewInlineKeyboardButtonData("Blue", "blue"),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Yellow", "yellow"),
+		tgbotapi.NewInlineKeyboardButtonData("Orange", "orange"),
+		tgbotapi.NewInlineKeyboardButtonData("Gray", "gray"),
+	),
+)
+
 func Color(
 	comm comm.Comm,
 	send chan<- tgbotapi.Chattable,
@@ -13,7 +26,9 @@ func Color(
 	for {
 		select {
 		case <-comm.Quit:
+			// For now, simply quit. Later, we'll want to add all the information in the db
 			return
+
 		case update := <-comm.Updates:
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "colors everywhere")
 			msg.ReplyToMessageID = update.Message.MessageID
