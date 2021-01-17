2e6868d66cb214f68c13b7203a1fe0cf2f220ad8
Guy-Laurent Subri <guy-laurent@subri.ch>
2021-01-17 23:41:02 +0100

    Add rating keyboard

---
+++ pkg/commands/keyboards/rating.go
@@ -0,0 +1,16 @@
+package keyboards
+
+// The RatingChoices represent the range of ratings with wich a user can rate
+// the enjoyability of a route
+var RatingChoices = []Choice{
+	{Label: "1", Action: "1"},
+	{Label: "2", Action: "2"},
+	{Label: "3", Action: "3"},
+	{Label: "4", Action: "4"},
+	{Label: "5", Action: "5"},
+	{Label: "6", Action: "6"},
+	{Label: "7", Action: "7"},
+	{Label: "8", Action: "8"},
+	{Label: "9", Action: "9"},
+	{Label: "10", Action: "10"},
+}
