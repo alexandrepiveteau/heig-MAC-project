f342d4c551b97bb3e3e468065c3f3300c2ef1772
Guy-Laurent Subri <guy-laurent@subri.ch>
2021-01-17 13:33:17 +0100

    Add createInMongo for an Attempt

--- pkg/types/attempt.go
+++ pkg/types/attempt.go
@@ -1,9 +1,11 @@
 package types
 
 import (
+	"context"
 	"errors"
 
 	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -25,4 +27,43 @@ func (a *Attempt) Store(
 	neo4jDriver neo4j.Driver,
 ) (primitive.ObjectID, error) {
 	return primitive.NewObjectID(), errors.New("Not implemented")
+}
+
+func (a *Attempt) createInMongo(
+	db *mongo.Database,
+	neo4jDriver neo4j.Driver,
+) (primitive.ObjectID, error) {
+
+	gymId, err := GymGetId(db, a.GymName)
+	if err != nil {
+		return primitive.NewObjectID(), err
+	}
+
+	routeId, err := RouteGetId(db, gymId, a.RouteName)
+	if err != nil {
+		return primitive.NewObjectID(), err
+	}
+
+	// Add route
+	id, err := routeCollection(db).InsertOne(
+		context.TODO(),
+		bson.D{
+			{Key: "gym", Value: gymId},
+			{Key: "route", Value: routeId},
+			{Key: "proposedGrade", Value: a.ProposedGrade},
+			{Key: "performance", Value: a.Performance},
+		},
+	)
+
+	if err != nil {
+		return primitive.NewObjectID(), err
+	}
+
+	// Assert type ObjectID
+	objectId, ok := id.InsertedID.(primitive.ObjectID)
+	if !ok {
+		return primitive.NewObjectID(), errors.New("ObjectID was not found.")
+	}
+
+	return objectId, nil
 }
