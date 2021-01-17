f61f5f79078018647561a5690cfc662c50a6f240
Alexandre Piveteau <alexandre@piveteau.email>
2021-01-17 13:48:19 +0100

    Add remaining attempt stages

--- pkg/commands/climb_route.go
+++ pkg/commands/climb_route.go
@@ -1,11 +1,13 @@
 package commands
 
 import (
+	"climb/pkg/commands/keyboards"
 	"climb/pkg/types"
 	"climb/pkg/utils"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 	"go.mongodb.org/mongo-driver/mongo"
+	"log"
 )
 
 type climbRouteStage int
@@ -15,7 +17,7 @@ const (
 	climbRouteGym
 	climbRouteRoute
 	climbRoutePerformance
-	climbRouteRating
+	climbRouteGrade
 	climbRouteEnd
 )
 
@@ -31,16 +33,77 @@ type climbRouteState struct {
 	gym         *string
 	route       *string
 	performance *string
-	rating      *int
+	grade       *string
 }
 
 func (s *climbRouteState) init(update tgbotapi.Update) {
 	msg1 := tgbotapi.NewMessage(utils.GetChatId(&update), "Adding a new attempt to an existing route.")
 	msg2 := tgbotapi.NewMessage(utils.GetChatId(&update), "In which gym are you climbing?")
 
-	s.bot.Send(msg1)
-	s.bot.Send(msg2)
+	_, _ = s.bot.Send(msg1)
+	_, _ = s.bot.Send(msg2)
 	s.stage = climbRouteGym
+}
+
+func (s *climbRouteState) rcvGym(update tgbotapi.Update) {
+	data := update.Message.Text
+	s.gym = &data
+
+	msg := tgbotapi.NewMessage(utils.GetChatId(&update), "What is the name of the route?")
+
+	_, _ = s.bot.Send(msg)
+	s.stage = climbRouteRoute
+}
+
+func (s *climbRouteState) rcvRoute(update tgbotapi.Update) {
+	data := update.Message.Text
+	s.route = &data
+
+	msg := tgbotapi.NewMessage(utils.GetChatId(&update), "What was your performance?")
+	msg.ReplyMarkup = keyboards.Performance
+
+	_, _ = s.bot.Send(msg)
+	s.stage = climbRoutePerformance
+}
+
+func (s *climbRouteState) rcvPerformance(update tgbotapi.Update) {
+	data := update.CallbackQuery.Data
+	s.performance = &data
+
+	utils.RemoveInlineKeyboard(s.bot, &update)
+
+	msg := tgbotapi.NewMessage(utils.GetChatId(&update), "How would you grade the route?")
+	msg.ReplyMarkup = keyboards.Grade
+
+	_, _ = s.bot.Send(msg)
+	s.stage = climbRouteGrade
+}
+
+func (s *climbRouteState) rcvGrade(update tgbotapi.Update) {
+	data := update.CallbackQuery.Data
+	s.grade = &data
+
+	utils.RemoveInlineKeyboard(s.bot, &update)
+
+	msg := tgbotapi.NewMessage(utils.GetChatId(&update), "Long live the swollen forearms! 💪")
+	_, _ = s.bot.Send(msg)
+	s.stage = climbRouteEnd
+}
+
+func (s *climbRouteState) save() {
+	attempt := types.Attempt{
+		GymName:       *s.gym,
+		RouteName:     *s.route,
+		ProposedGrade: *s.grade,
+		Performance:   *s.performance,
+	}
+
+	log.Printf("Saving attempt %+v\n", attempt)
+
+	_, err := attempt.Store(s.mongodb, s.neo4jDriver)
+	if err != nil {
+		log.Printf("Error saving Attempt: %s\n", err.Error())
+	}
 }
 
 func ClimbRouteCmd(
@@ -62,15 +125,32 @@ func ClimbRouteCmd(
 	for {
 		select {
 		case <-comm.StopCommand:
-			// TODO : Save data in db, then quit
+			// Save data in db, then quit
+			state.save()
 			return
 		case update := <-comm.Updates:
 			switch state.stage {
 			case climbRouteInit:
 				state.init(update)
 				break
+			case climbRouteGym:
+				state.rcvGym(update)
+				break
+			case climbRouteRoute:
+				state.rcvRoute(update)
+				break
+			case climbRoutePerformance:
+				state.rcvPerformance(update)
+				break
+			case climbRouteGrade:
+				state.rcvGrade(update)
+				commandTermination <- struct{}{}
+				break
+			case climbRouteEnd:
+				break
 			default:
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Sorry I'm lost.")
+				id := utils.GetChatId(&update)
+				msg := tgbotapi.NewMessage(id, "Sorry I'm lost.")
 				_, _ = bot.Send(msg)
 				break
 			}
