b48c5f8d5bd1f2ce3422280e11c866376ca4697b
Guy-Laurent Subri <guy-laurent@subri.ch>
2021-01-17 11:11:26 +0100

    Link route with gym in neo4j

--- pkg/types/route.go
+++ pkg/types/route.go
@@ -30,10 +30,14 @@ func (r *Route) Store(
 	}
 
 	// 2. Create in Neo4j
-	err = r.createInNeo4j(neo4jDriver)
+	err = r.createInNeo4j(neo4jDriver, id.String())
 	if err != nil {
 		return primitive.NewObjectID(), err
 	}
+
+	// 3. Link with Gym
+	gym := Gym{Name: r.Gym} // Don't need to ask for it since we can create it
+	err = r.linkWith(neo4jDriver, id.String(), gym)
 
 	// Return mongo's id
 	return id, nil
@@ -81,7 +85,7 @@ func (r *Route) createInMongo(
 	return objectId, nil
 }
 
-func (r *Route) createInNeo4j(driver neo4j.Driver) error {
+func (r *Route) createInNeo4j(driver neo4j.Driver, id string) error {
 	session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
 	defer session.Close()
 
@@ -89,6 +93,7 @@ func (r *Route) createInNeo4j(driver neo4j.Driver) error {
 
 		cypher := `CREATE (r:Route)
 							SET r = {
+							  id: $id,
 								name: $name,
 								grade: $grade,
 								holds: $holds
@@ -96,10 +101,42 @@ func (r *Route) createInNeo4j(driver neo4j.Driver) error {
 							RETURN r`
 
 		params := map[string]interface{}{
+			"id":    id,
 			"name":  r.Name,
 			"grade": r.Grade,
 			"holds": r.Holds,
 			//"setDate": r.SetDate, TODO: add date
+		}
+
+		transRes, err := transaction.Run(cypher, params)
+		if err != nil {
+			return nil, err
+		}
+		return transRes, nil
+	})
+
+	return err
+}
+
+func (r *Route) linkWith(
+	driver neo4j.Driver,
+	id string,
+	gym Gym,
+) error {
+
+	session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
+	defer session.Close()
+
+	_, err := session.WriteTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
+
+		cypher := `MATCH (r:Route) WHERE r.id = $rId
+							MATCH (g:Gym) WHERE g.name = $gName
+							CREATE (r)-[:IS_IN]->(g)
+							RETURN r`
+
+		params := map[string]interface{}{
+			"rId":   id,
+			"gName": gym.Name,
 		}
 
 		transRes, err := transaction.Run(cypher, params)
